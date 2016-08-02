ffebf9a73839738d110849cc1c70153a2d29a725
Doomtrain14 <yet.ebreo@gmail.com>
2016-08-02 20:43:09 +0800

    Initial Commit

---
+++ krash/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lb_inputs = New System.Windows.Forms.ListBox()
+        Me.cmd_removeinput = New System.Windows.Forms.Button()
+        Me.cmd_addinput = New System.Windows.Forms.Button()
+        Me.bw_extracttests = New System.ComponentModel.BackgroundWorker()
+        Me.gb_testblocks = New System.Windows.Forms.GroupBox()
+        Me.cmd_clear = New System.Windows.Forms.Button()
+        Me.lbl_selected_test = New System.Windows.Forms.Label()
+        Me.lb_testblocks = New System.Windows.Forms.ListBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.sb_status = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.cmd_krash = New System.Windows.Forms.Button()
+        Me.bw_krash = New System.ComponentModel.BackgroundWorker()
+        Me.gb_jmp = New System.Windows.Forms.GroupBox()
+        Me.chk_jmp = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.gb_testblocks.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lb_inputs)
+        Me.GroupBox1.Controls.Add(Me.cmd_removeinput)
+        Me.GroupBox1.Controls.Add(Me.cmd_addinput)
+        Me.GroupBox1.Location = New System.Drawing.Point(7, 4)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(177, 215)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Input files"
+        '
+        'lb_inputs
+        '
+        Me.lb_inputs.FormattingEnabled = True
+        Me.lb_inputs.Location = New System.Drawing.Point(6, 20)
+        Me.lb_inputs.Name = "lb_inputs"
+        Me.lb_inputs.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lb_inputs.Size = New System.Drawing.Size(164, 160)
+        Me.lb_inputs.TabIndex = 3
+        '
+        'cmd_removeinput
+        '
+        Me.cmd_removeinput.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmd_removeinput.Location = New System.Drawing.Point(117, 185)
+        Me.cmd_removeinput.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.cmd_removeinput.Name = "cmd_removeinput"
+        Me.cmd_removeinput.Size = New System.Drawing.Size(54, 22)
+        Me.cmd_removeinput.TabIndex = 2
+        Me.cmd_removeinput.Text = "&Remove"
+        Me.cmd_removeinput.UseVisualStyleBackColor = True
+        '
+        'cmd_addinput
+        '
+        Me.cmd_addinput.Location = New System.Drawing.Point(6, 185)
+        Me.cmd_addinput.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.cmd_addinput.Name = "cmd_addinput"
+        Me.cmd_addinput.Size = New System.Drawing.Size(54, 22)
+        Me.cmd_addinput.TabIndex = 1
+        Me.cmd_addinput.Text = "&Add"
+        Me.cmd_addinput.UseVisualStyleBackColor = True
+        '
+        'bw_extracttests
+        '
+        Me.bw_extracttests.WorkerReportsProgress = True
+        Me.bw_extracttests.WorkerSupportsCancellation = True
+        '
+        'gb_testblocks
+        '
+        Me.gb_testblocks.Controls.Add(Me.cmd_clear)
+        Me.gb_testblocks.Controls.Add(Me.lbl_selected_test)
+        Me.gb_testblocks.Controls.Add(Me.lb_testblocks)
+        Me.gb_testblocks.Location = New System.Drawing.Point(190, 4)
+        Me.gb_testblocks.Margin = New System.Windows.Forms.Padding(4)
+        Me.gb_testblocks.Name = "gb_testblocks"
+        Me.gb_testblocks.Padding = New System.Windows.Forms.Padding(4)
+        Me.gb_testblocks.Size = New System.Drawing.Size(177, 215)
+        Me.gb_testblocks.TabIndex = 1
+        Me.gb_testblocks.TabStop = False
+        Me.gb_testblocks.Text = "Testblocks"
+        '
+        'cmd_clear
+        '
+        Me.cmd_clear.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmd_clear.Location = New System.Drawing.Point(116, 185)
+        Me.cmd_clear.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.cmd_clear.Name = "cmd_clear"
+        Me.cmd_clear.Size = New System.Drawing.Size(54, 22)
+        Me.cmd_clear.TabIndex = 3
+        Me.cmd_clear.Text = "&Clear"
+        Me.cmd_clear.UseVisualStyleBackColor = True
+        '
+        'lbl_selected_test
+        '
+        Me.lbl_selected_test.AutoSize = True
+        Me.lbl_selected_test.Location = New System.Drawing.Point(4, 190)
+        Me.lbl_selected_test.Name = "lbl_selected_test"
+        Me.lbl_selected_test.Size = New System.Drawing.Size(94, 13)
+        Me.lbl_selected_test.TabIndex = 2
+        Me.lbl_selected_test.Text = "0 Item(s) selected"
+        '
+        'lb_testblocks
+        '
+        Me.lb_testblocks.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lb_testblocks.FormattingEnabled = True
+        Me.lb_testblocks.Location = New System.Drawing.Point(7, 20)
+        Me.lb_testblocks.Margin = New System.Windows.Forms.Padding(4)
+        Me.lb_testblocks.Name = "lb_testblocks"
+        Me.lb_testblocks.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.lb_testblocks.Size = New System.Drawing.Size(163, 160)
+        Me.lb_testblocks.TabIndex = 1
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.sb_status})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 484)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Padding = New System.Windows.Forms.Padding(1, 0, 17, 0)
+        Me.StatusStrip1.Size = New System.Drawing.Size(373, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'sb_status
+        '
+        Me.sb_status.AutoSize = False
+        Me.sb_status.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.sb_status.Name = "sb_status"
+        Me.sb_status.Size = New System.Drawing.Size(230, 17)
+        Me.sb_status.Text = "Status: Idle/Ready"
+        Me.sb_status.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'cmd_krash
+        '
+        Me.cmd_krash.Location = New System.Drawing.Point(292, 459)
+        Me.cmd_krash.Name = "cmd_krash"
+        Me.cmd_krash.Size = New System.Drawing.Size(75, 22)
+        Me.cmd_krash.TabIndex = 3
+        Me.cmd_krash.Text = "Krash"
+        Me.cmd_krash.UseVisualStyleBackColor = True
+        '
+        'bw_krash
+        '
+        Me.bw_krash.WorkerReportsProgress = True
+        Me.bw_krash.WorkerSupportsCancellation = True
+        '
+        'gb_jmp
+        '
+        Me.gb_jmp.Location = New System.Drawing.Point(7, 226)
+        Me.gb_jmp.Name = "gb_jmp"
+        Me.gb_jmp.Size = New System.Drawing.Size(360, 227)
+        Me.gb_jmp.TabIndex = 4
+        Me.gb_jmp.TabStop = False
+        Me.gb_jmp.Text = "JMP"
+        '
+        'chk_jmp
+        '
+        Me.chk_jmp.AutoSize = True
+        Me.chk_jmp.Checked = True
+        Me.chk_jmp.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chk_jmp.Location = New System.Drawing.Point(7, 460)
+        Me.chk_jmp.Name = "chk_jmp"
+        Me.chk_jmp.Size = New System.Drawing.Size(80, 17)
+        Me.chk_jmp.TabIndex = 5
+        Me.chk_jmp.Text = "Enable JMP"
+        Me.chk_jmp.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(373, 506)
+        Me.Controls.Add(Me.chk_jmp)
+        Me.Controls.Add(Me.gb_jmp)
+        Me.Controls.Add(Me.cmd_krash)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.gb_testblocks)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Krash 0.0.1a"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.gb_testblocks.ResumeLayout(False)
+        Me.gb_testblocks.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents cmd_removeinput As System.Windows.Forms.Button
+    Friend WithEvents cmd_addinput As System.Windows.Forms.Button
+    Friend WithEvents bw_extracttests As System.ComponentModel.BackgroundWorker
+    Friend WithEvents gb_testblocks As System.Windows.Forms.GroupBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents sb_status As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents lb_testblocks As System.Windows.Forms.ListBox
+    Friend WithEvents lb_inputs As System.Windows.Forms.ListBox
+    Friend WithEvents cmd_clear As System.Windows.Forms.Button
+    Friend WithEvents lbl_selected_test As System.Windows.Forms.Label
+    Friend WithEvents cmd_krash As System.Windows.Forms.Button
+    Friend WithEvents bw_krash As System.ComponentModel.BackgroundWorker
+    Friend WithEvents gb_jmp As System.Windows.Forms.GroupBox
+    Friend WithEvents chk_jmp As System.Windows.Forms.CheckBox
+
+End Class
---
+++ krash/Form1.vb
@@ -0,0 +1,232 @@
+﻿Imports System.Text.RegularExpressions
+
+
+Public Class Form1
+
+    Dim g_test_blocks As New Hashtable()
+    Dim g_krash_test_blocks As New Hashtable()
+
+    Dim g_input_files As New Hashtable()
+    Dim g_curr_input_files As New Hashtable()
+    Dim g_curr_filelength As Long
+    Dim g_byteread As Long
+
+    Dim g_lot As String
+    Dim g_wafer As String
+    Dim g_reticle As String
+    Dim g_die_x As String
+    Dim g_die_y As String
+    Dim g_temp As String
+    Dim g_test_header As String
+
+    Private Sub cmd_addinput_Click(sender As Object, e As EventArgs) Handles cmd_addinput.Click
+        Dim dg_open As New OpenFileDialog
+        dg_open.Multiselect = True
+        dg_open.Filter = "Datalog files (*.datalog)|*.datalog|Raw files (*.raw)|*.raw|All files (*.*)|*.*"
+        If (dg_open.ShowDialog() = Windows.Forms.DialogResult.Cancel) Then
+            Exit Sub
+        End If
+        g_curr_input_files.Clear()
+        For Each file As String In dg_open.FileNames
+            Dim m_key As String = System.IO.Path.GetFileName(file)
+            If Not g_input_files.ContainsValue(file) Then
+                lb_inputs.Items.Add(m_key)
+                g_input_files.Add(m_key, file)
+                g_curr_input_files.Add(m_key, file)
+            End If
+        Next
+
+        cmd_addinput.Enabled = False
+        cmd_removeinput.Enabled = False
+        bw_extracttests.RunWorkerAsync()
+
+    End Sub
+
+    Private Sub cmd_removeinput_Click(sender As Object, e As EventArgs) Handles cmd_removeinput.Click
+
+
+        Do While (lb_inputs.SelectedItems.Count > 0)
+            g_input_files.Remove(lb_inputs.SelectedItem.ToString)
+            lb_inputs.Items.Remove(lb_inputs.SelectedItem)
+        Loop
+
+    End Sub
+
+
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If (bw_extracttests.IsBusy) Then
+            bw_extracttests.CancelAsync()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub bw_extracttests_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bw_extracttests.DoWork
+        'For Each file As String In g_input_files
+
+        For Each file As DictionaryEntry In g_curr_input_files
+            If System.IO.File.Exists(file.Value) = True Then
+                g_curr_filelength = New System.IO.FileInfo(file.Value).Length
+                g_byteread = 0
+
+                Dim modulo As Long
+                Dim objReader As New System.IO.StreamReader(file.Value.ToString)
+                Dim strLine As String
+                Do While objReader.Peek() <> -1
+                    If (bw_extracttests.CancellationPending = True) Then
+                        e.Cancel = True
+                        Exit Sub
+                    End If
+                    strLine = objReader.ReadLine()
+                    Dim firstword As String
+                    firstword = (strLine.Split(" "))(0)
+                    If (String.Compare(firstword, "TNAME") And String.Compare(firstword.Substring(0, 1), "#")) Then
+                        If (Not g_test_blocks.ContainsKey(firstword)) Then
+                            g_test_blocks.Add(firstword, 1)
+                        End If
+                    End If
+                    g_byteread += (strLine.Length + 2)
+
+                    If (modulo Mod 1024 = 0) Then
+                        bw_extracttests.ReportProgress(Convert.ToInt32((g_byteread / g_curr_filelength) * 100))
+                        modulo = 0
+                    End If
+                    modulo += 1
+                Loop
+
+                objReader.Close()
+            End If
+        Next
+    End Sub
+
+    Private Sub bw_extracttests_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles bw_extracttests.ProgressChanged
+        sb_status.Text = "Status: Reading input file(s) - " & e.ProgressPercentage & "%"
+
+    End Sub
+
+    Private Sub bw_extracttests_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles bw_extracttests.RunWorkerCompleted
+        lb_testblocks.Items.Clear()
+        Dim keys As New List(Of String)
+        keys.AddRange(From i As String In g_test_blocks.Keys)
+        keys.Sort()
+        For Each k In keys
+            lb_testblocks.Items.Add(k)
+        Next k
+        sb_status.Text = "Status: Idle/Ready"
+        cmd_addinput.Enabled = True
+        cmd_removeinput.Enabled = True
+    End Sub
+
+    Private Sub cmd_clear_Click(sender As Object, e As EventArgs) Handles cmd_clear.Click
+        lb_testblocks.SelectedItems.Clear()
+    End Sub
+
+    Private Sub lb_testblocks_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lb_testblocks.SelectedIndexChanged
+        lbl_selected_test.Text = lb_testblocks.SelectedItems.Count & " Item(s) selected"
+        g_krash_test_blocks.Clear()
+
+        For i = 0 To lb_testblocks.SelectedItems.Count - 1
+            If Not g_krash_test_blocks.ContainsKey(lb_testblocks.SelectedItems(i)) Then
+                g_krash_test_blocks.Add(lb_testblocks.SelectedItems(i), "1")
+            End If
+        Next
+    End Sub
+
+    Private Sub cmd_krash_Click(sender As Object, e As EventArgs) Handles cmd_krash.Click
+        lb_inputs.Enabled = False
+        lb_testblocks.Enabled = False
+        cmd_addinput.Enabled = False
+        cmd_removeinput.Enabled = False
+        cmd_krash.Enabled = False
+        cmd_clear.Enabled = False
+
+
+        bw_krash.RunWorkerAsync()
+
+
+    End Sub
+
+    Private Sub bw_krash_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bw_krash.DoWork
+        For Each file As DictionaryEntry In g_input_files
+            Dim headerflag As Boolean = False
+            If System.IO.File.Exists(file.Value) = True Then
+                For Each testblock As DictionaryEntry In g_krash_test_blocks
+                    Dim objWriter As New System.IO.StreamWriter(System.IO.Path.GetDirectoryName(file.Value) & "\krash_" & testblock.Key & ".csv", True)
+                    Dim objReader As New System.IO.StreamReader(file.Value.ToString)
+
+
+                    Dim strLine As String
+                    Dim strBuff As New System.Text.StringBuilder()
+
+                    g_curr_filelength = New System.IO.FileInfo(file.Value).Length
+                    g_byteread = 0
+                    Dim modulo As Long
+                    'Default
+                    g_wafer = "99"
+                    g_reticle = "99"
+
+                    Do While objReader.Peek() <> -1
+                        strLine = objReader.ReadLine()
+                        g_byteread += (strLine.Length + 2)
+
+                        If (modulo Mod 1024 = 0) Then
+                            bw_krash.ReportProgress(Convert.ToInt32((g_byteread / g_curr_filelength) * 100))
+                            modulo = 0
+                        End If
+                        modulo += 1
+
+                        If strLine.Substring(0, 4) = "#LOT" Then
+                            g_lot = (strLine.Split(" "))(1)
+                        ElseIf strLine.Substring(0, 5) = "TNAME" Then
+                            g_test_header = strLine
+                        ElseIf strLine.Substring(0, 5) = "#TEMP" Then
+                            g_temp = (strLine.Split(" "))(1)
+                        ElseIf strLine.Substring(0, 6) = "#WAFER" Then
+                            g_wafer = (strLine.Split(" "))(1)
+                        ElseIf strLine.Substring(0, 6) = "#DIE_X" Then
+                            g_die_x = (strLine.Split(" "))(1)
+                        ElseIf strLine.Substring(0, 6) = "#DIE_Y" Then
+                            g_die_y = (strLine.Split(" "))(1)
+                        ElseIf strLine.Substring(0, 8) = "#RETICLE" Then
+                            g_reticle = (strLine.Split(" "))(1)
+                        ElseIf strLine.Length >= testblock.Key.ToString.Length Then
+                            If strLine.Substring(0, testblock.Key.ToString.Length) = testblock.Key Then
+                                If headerflag = False Then
+                                    headerflag = True
+                                    objWriter.WriteLine("LOT,WAFER,RETICLE,DIE_X,DIE_Y,TEMP," & g_test_header.Replace(" ", ",").Trim(","))
+                                End If
+                                objWriter.WriteLine(g_lot & "," & g_wafer & "," & g_reticle & "," & g_die_x & "," & g_die_y & "," & g_temp & "," & strLine.Replace(" ", ",").Trim(","))
+                            End If
+                        End If
+
+                    Loop
+
+                    objReader.Close()
+                    objWriter.Close()
+                Next
+            End If
+        Next
+    End Sub
+
+    Private Sub bw_krash_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles bw_krash.ProgressChanged
+        sb_status.Text = "Status: Krashing input file(s) - " & e.ProgressPercentage & "%"
+    End Sub
+
+    Private Sub bw_krash_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles bw_krash.RunWorkerCompleted
+        lb_inputs.Enabled = True
+        lb_testblocks.Enabled = True
+        cmd_addinput.Enabled = True
+        cmd_removeinput.Enabled = True
+        cmd_krash.Enabled = True
+        cmd_clear.Enabled = True
+
+        sb_status.Text = "Status: Idle/Ready"
+    End Sub
+
+    Private Sub chk_jmp_CheckedChanged(sender As Object, e As EventArgs) Handles chk_jmp.CheckedChanged
+        gb_jmp.Enabled = chk_jmp.Checked
+    End Sub
+End Class
